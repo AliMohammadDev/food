ca7ee6b381bed45e7be7e729edd47a3eedb5a09d
alishadel2001 <alishadel2001@gmail.com>
2025-06-10 12:35:56 +0300

    fixed in header when show item depoedn on category default Salad

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,13 +3,37 @@ import Header from "../../components/Header/Header";
 import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
 import AppDownload from "../../components/AppDownload/AppDownload";
 import { useState } from "react";
+import { useGetCategories } from "../../api/category";
+import { useEffect } from "react";
+import Skeleton from "../../components/Skeleton";
 function Home() {
   const [selectedItems, setSelectedItems] = useState(null);
+  const { data: menu, isLoading, isError } = useGetCategories();
+
+  useEffect(() => {
+    if (menu && menu.length > 0) {
+      const saladCategory = menu.find(
+        (cat) => cat.name.toLowerCase() === "salad"
+      );
+      if (saladCategory) {
+        setSelectedItems(saladCategory.items);
+      } else {
+        setSelectedItems(menu[0].items);
+      }
+    }
+  }, [menu]);
+
+  if (isLoading) return <Skeleton />;
+  if (isError)
+    return (
+      <div className="text-center text-red-500">Error loading categories</div>
+    );
 
   return (
     <div className="py-4 px-10">
       <Header />
       <ExploreMenu onCategorySelect={(items) => setSelectedItems(items)} />
+
       <FoodDisplay items={selectedItems} />
       <AppDownload />
     </div>
