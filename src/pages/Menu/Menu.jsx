e2c89cac0d317440cc859f5f5879d03577f801a2
alishadel2001 <alishadel2001@gmail.com>
2025-06-10 12:03:20 +0300

    fixed in menu page

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -15,17 +15,20 @@ function Menu() {
       </h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8">
-        {items.map((item) => (
-          <FoodItem
-            key={item.id}
-            id={item.id}
-            name={item.name}
-            image={item.image}
-            description={item.description}
-            price={item.price}
-            category={item.category}
-          />
-        ))}
+        {items.map((item) => {
+          const image = item.image.replace("http://localhost:3000/uploads/items", "");
+          return (
+            <FoodItem
+              key={item.id}
+              id={item.id}
+              name={item.name}
+              image={image}
+              description={item.description}
+              price={item.price}
+              category={item.category}
+            />
+          );
+        })}
       </div>
     </div>
   );
